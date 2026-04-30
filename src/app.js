30801ab31aced171fc500e90905a431d24dc00a5
JohwSantos <jhoewsantos@gmail.com>
2026-04-29 22:17:57 -0300

    Adicionamos a pasta routes e o arquivo clienteRoutes.js, Adicionei linhas no app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const clientesRoutes = require("./routes/clientesRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//Rota raiz:
+
+app.get("/", (req,res) => {
+    res.json({
+        mensagem:"API de Clientes",
+        versao: "1.0.0",
+        endpoints: {
+            listarTodos: "GET /clientes",
+            buscarPorId: "GET /clientes/:id"
+        }
+    })
+})
+
+app.use("/clientes", clientesRoutes);
+
+app.use((req,res) =>{
+    res.status(404).json({
+        sucesso: false,
+        mensagem: "Rota não encontrada",
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+module.exports = app;
